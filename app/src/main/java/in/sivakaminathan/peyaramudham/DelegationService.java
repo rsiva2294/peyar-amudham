3c20120219644cb68e7e966a35e1dbfcd39758dd
rsiva2294 <rsiva2294@gmail.com>
2026-04-10 18:48:29 +0530

    feat: package Android app via Bubblewrap, update manifest, and add TWA digital asset links

---
+++ app/src/main/java/in/sivakaminathan/peyaramudham/DelegationService.java
@@ -0,0 +1,14 @@
+package in.sivakaminathan.peyaramudham;
+
+
+
+public class DelegationService extends
+        com.google.androidbrowserhelper.trusted.DelegationService {
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+        
+    }
+}
+
